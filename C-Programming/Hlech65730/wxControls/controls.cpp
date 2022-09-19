8e1910105ad21d76c62ad2769cab553435f5f693
uid02589 <Hlech65730@aol.com>
2022-09-19 15:51:28 +0200

    uid02589: C-Programming - wxControls adapting from controls.cpp (2)

--- C-Programming/Hlech65730/wxControls/controls.cpp
+++ C-Programming/Hlech65730/wxControls/controls.cpp
@@ -12,31 +12,3 @@
     #pragma hdrstop
 #endif
 
-#if wxUSE_TOOLTIPS
-    #include "wx/tooltip.h"
-#ifdef __WXMSW__
-    #include "wx/numdlg.h"
-#endif // __WXMSW__
-#endif // wxUSE_TOOLTIPS
-
-
-#ifndef wxUSE_SPINBTN
-    #define wxUSE_SPINBTN 1
-#endif
-
-#include "wx/progdlg.h"
-
-#if wxUSE_SPINCTRL
-    #include "wx/spinctrl.h"
-#endif // wxUSE_SPINCTRL
-
-#if !wxUSE_TOGGLEBTN
-    #define wxToggleButton wxCheckBox
-    #define EVT_TOGGLEBUTTON EVT_CHECKBOX
-#endif
-
-#ifndef wxHAS_IMAGES_IN_RESOURCES
-    #include "../sample.xpm"
-#endif
-
-
--- C-Programming/Hlech65730/wxControls/wxControlsMain.h
+++ C-Programming/Hlech65730/wxControls/wxControlsMain.h
@@ -28,6 +28,32 @@
 #include <wx/button.h>
 #include <wx/statline.h>
 
+#if wxUSE_TOOLTIPS
+    #include "wx/tooltip.h"
+#ifdef __WXMSW__
+    #include "wx/numdlg.h"
+#endif // __WXMSW__
+#endif // wxUSE_TOOLTIPS
+
+#ifndef wxUSE_SPINBTN
+    #define wxUSE_SPINBTN 1
+#endif
+
+#include "wx/progdlg.h"
+
+#if wxUSE_SPINCTRL
+    #include "wx/spinctrl.h"
+#endif // wxUSE_SPINCTRL
+
+#if !wxUSE_TOGGLEBTN
+    #define wxToggleButton wxCheckBox
+    #define EVT_TOGGLEBUTTON EVT_CHECKBOX
+#endif
+
+#ifndef wxHAS_IMAGES_IN_RESOURCES
+    #include "../sample.xpm"
+#endif
+
 #ifndef __WXMSW__
     #include "icons/choice.xpm"
     #include "icons/combo.xpm"
